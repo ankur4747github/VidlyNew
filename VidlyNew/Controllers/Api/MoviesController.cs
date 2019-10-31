85e17ab5e456f8adaf66139f25586f7fa187feb3
Ankur Tripathi <ankur456799@gmail.com>
2019-10-31 20:58:37 +0530

    Added API for movie and customers

---
+++ VidlyNew/Controllers/Api/MoviesController.cs
@@ -0,0 +1,91 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Vidly.Models;
+using VidlyNew.Dtos;
+using VidlyNew.Models;
+
+namespace VidlyNew.Controllers.Api
+{
+    public class MoviesController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public MoviesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        //GET /api/movies
+        public IEnumerable<MovieDto> GetMovies()
+        {
+            return _context.Movies.ToList().Select(Mapper.Map<Movie, MovieDto>);
+        }
+
+        //GET /api/movies/1
+        public IHttpActionResult GetMovie(int id)
+        {
+            var movie = _context.Movies.SingleOrDefault(i=>i.Id == id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(Mapper.Map<Movie, MovieDto>(movie));
+        }
+
+        // POST /api/movies
+        [HttpPost]
+        public IHttpActionResult CreateMovie(MovieDto movieDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+            var movie = Mapper.Map<MovieDto, Movie>(movieDto);
+            _context.Movies.Add(movie);
+            _context.SaveChanges();
+            movieDto.Id = movie.Id;
+
+            return Created(new Uri(Request.RequestUri + "/" + movie.Id), movieDto);
+        }
+
+        //PUT //api/movies/1
+        [HttpPut]
+        public void UpdateMovie(int id,MovieDto movieDto)
+        {
+            var movie = _context.Movies.SingleOrDefault(i => i.Id == id);
+            if (movie == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+            }
+            var movieInDb = _context.Movies.SingleOrDefault(i => i.Id == id);
+            if (movieInDb == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+
+            Mapper.Map(movieDto, movieInDb);
+            movieInDb.Id = id;
+            _context.SaveChanges();
+        }
+
+        //DELETE /api/customers/1
+        [HttpDelete]
+        public void DeleteMovie(int id)
+        {
+            var movieInDb = _context.Movies.SingleOrDefault(i => i.Id == id);
+            if (movieInDb == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+
+            _context.Movies.Remove(movieInDb);
+            _context.SaveChanges();
+        }
+    }
+}
---
+++ VidlyNew/Dtos/CustomerDto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using VidlyNew.Models;
+
+namespace VidlyNew.Dtos
+{
+    public class CustomerDto
+    {
+        public int Id { get; set; }
+
+        //Data Annotations
+
+        [Required(ErrorMessage = "Please enter customer's name")]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        public bool IsSubscriedNewsLetter { get; set; }
+
+        public byte MembershipTypeId { get; set; }
+
+        //[Min18YearsIfAMember] for IHttpActionResultInApi
+        public DateTime? Birthdata { get; set; }
+    }
+}
